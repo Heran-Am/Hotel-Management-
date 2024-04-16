3d1c2a1f484238df4bc5dc57f6abf148ea2a52e1
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-04-16 22:42:51 +0200

    Update recipt.py

--- recipt.py
+++ recipt.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-import tkinter as tk
 
-try:
-    from tkinter import *
-    # Import only needed names or import the module and then use its members.
-except ImportError:
-    from tkinter import *
+import tkinter as tk
 
 class Receipt:
     def __init__(self):
@@ -19,7 +13,6 @@ class Receipt:
             list1[2] = list1[2][:-1]
             list1[3] = list1[3][:-1]
             list1[4] = list1[4][:-1]
-
             p = '''
             @@@@@@@@@@@  PROJECTWORLDS HOTEL AND RESORTS  @@@@@@@@@@@@@
             @@@@@@@@@@@@ BHILAI CHHATTISGARH@@@@@@@@@@@@@@
@@ -31,16 +24,15 @@ class Receipt:
             MOBILE NO.-%s
             YOUR TOTAL BILL IS Rs.-%s
             YOUR ROOM NUMBER IS %s    
-
             ''' % (list1[0], list1[1], list1[2], list1[4], list1[3])
-
+        
         root = tk.Tk()
         root.geometry("800x800")
         root.title("Receipt")
         root.configure(background="#d9d9d9")
 
-        self.label = Label(root, background="#d9d9d9", text=p, disabledforeground="#a3a3a3", foreground="#000000",
-                           anchor=N, wraplength=1000, justify=LEFT)
+        self.label = tk.Label(root, background="#d9d9d9", text=p, disabledforeground="#a3a3a3", foreground="#000000",
+                           anchor=tk.N, wraplength=1000, justify=tk.LEFT)
         self.label.place(relx=0, rely=0, height=800, width=800)
 
         root.mainloop()
