313d07decf53115f6c759453357ffb6d44e2ec33
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-04-16 22:49:03 +0200

    Add files via upload

---
+++ DSL.py
@@ -0,0 +1,44 @@
+import pickle
+
+class HotelDataHandler:
+    @staticmethod
+    def save_guest_data(guest_data):
+        try:
+            with open("hotel.dat", "ab") as f:
+                pickle.dump(guest_data, f)
+        except FileNotFoundError:
+            print("Error: hotel.dat file not found.")
+
+    @staticmethod
+    def load_guest_data():
+        guest_data = []
+        try:
+            with open("hotel.dat", "rb") as f:
+                while True:
+                    try:
+                        guest = pickle.load(f)
+                        guest_data.append(guest)
+                    except EOFError:
+                        break
+        except FileNotFoundError:
+            print("Error: hotel.dat file not found.")
+        return guest_data
+
+    @staticmethod
+    def save_receipt_data(receipt_data):
+        try:
+            with open("receipt.txt", "w") as f:
+                for line in receipt_data:
+                    f.write(line + '\n')
+        except FileNotFoundError:
+            print("Error: receipt.txt file not found.")
+
+    @staticmethod
+    def load_receipt_data():
+        receipt_data = []
+        try:
+            with open("receipt.txt", "r") as f:
+                receipt_data = f.readlines()
+        except FileNotFoundError:
+            print("Error: receipt.txt file not found.")
+        return receipt_data
